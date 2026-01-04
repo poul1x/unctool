92710aa0903022840fd538cbb26389f63a142714
Pavel Knyazev <poulix.nova@mail.ru>
2026-01-05 02:56:25 +0300

    Update deployment script

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -8,29 +8,42 @@
 # Exit on error
 set -e
 
-latest_tag=$(git tag | grep unctool-cli | tail -n 1)
-rm -rf gh_release && mkdir gh_release
+rm -rf gh_release
+mkdir gh_release
 cd gh_release
 
-echo "Cloning repository with tag $latest_tag"
-git clone --depth 1 --branch $latest_tag \
-	https://github.com/poul1x/unctool.git
-
+echo "Cloning repository"
+git clone --depth 1 https://github.com/poul1x/unctool.git
 cd unctool
-echo "Building for x86_64..."
-rustup target add x86_64-unknown-linux-musl
-cargo build --release --target x86_64-unknown-linux-musl
-cp ./target/x86_64-unknown-linux-musl/release/unctool-cli unctool-x86_64
-strip --strip-all ./unctool-x86_64
 
-echo "Building for i686..."
+echo "Building CLI for Linux 64bit"
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl -p unctool-cli
+cp ./target/x86_64-unknown-linux-musl/release/unctool-cli unctool-cli-x64
+
+echo "Building CLI for Linux 32bit"
 rustup target add i686-unknown-linux-musl
-cargo build --release --target i686-unknown-linux-musl
-cp ./target/i686-unknown-linux-musl/release/unctool-cli unctool-i686
-strip --strip-all ./unctool-i686
+cargo build --release --target i686-unknown-linux-musl -p unctool-cli
+cp ./target/i686-unknown-linux-musl/release/unctool-cli unctool-cli-x32
+
+echo "Building GUI for Linux 64bit"
+rustup target add x86_64-unknown-linux-gnu
+cargo build --release --target x86_64-unknown-linux-gnu -p unctool-gui
+cp ./target/x86_64-unknown-linux-gnu/release/unctool-gui unctool-gui-x64
+
+echo "Building CLI for Linux 32bit"
+rustup target add i686-unknown-linux-gnu
+cargo build --release --target i686-unknown-linux-gnu -p unctool-gui
+cp ./target/i686-unknown-linux-gnu/release/unctool-gui unctool-gui-x32
 
 echo "Uploading files..."
-gh release create $latest_tag
-gh release upload $latest_tag ./unctool-x86_64
-gh release upload $latest_tag ./unctool-i686
+tag="$(git log --oneline -1 --format="%h")-$(date -u +%Y%m%d-%H%M)"
+gh release create $tag
+gh release upload $tag ./unctool-cli-x64
+gh release upload $tag ./unctool-cli-x32
+gh release upload $tag ./unctool-gui-x64
+gh release upload $tag ./unctool-gui-x32
+
+cd ..
+rm -rf gh_release
 echo "Done"
